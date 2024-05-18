539a643990b3e20633a227a3b7af359e48e3410f
Spencerguo11 <spencer.guo11@gmail.com>
2024-05-17 18:30:21 -0600

    added more files

--- edgarfacts/cmd/facts_main.go
+++ edgarfacts/cmd/facts_main.go
@@ -1,24 +1,20 @@
-// Declare Package
 package main
 
-// Make Imports
 import (
 	"fmt"
 
 	facts "github.com/spencerguo11/first_data_engineer_project/edgarfacts/internal/facts"
 )
 
-// Main Function
 func main() {
-	// Load Company Facts for Goldman Sachs
 	cik := "0000886982"
 	organization := "individual"
 	name := "Spencer Guo"
 	email := "spencerguo12@gmail.com"
-	facts, err := facts.LoadFacts(cik, organization, name, email)
+	companyFacts, err := facts.LoadFacts(cik, organization, name, email)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(facts))
+	fmt.Println(string(companyFacts))
 }
--- edgarfacts/go.mod
+++ edgarfacts/go.mod
@@ -1,3 +1,4 @@
 module github.com/spencerguo11/first_data_engineer_project/edgarfacts
 
 go 1.22.3
+
